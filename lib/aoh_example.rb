a3c3c034c28e25df7c6165bb51b79bf3a086209c
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-04 18:59:02 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,17 +7,17 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  constant_peeps = [
-    {:name =>"Don Gately", :occupation => "Live-in Staff"},
-    {:name =>"Joelle van Dyne", :occupation => "Radio Personality"}, 
-    {:name =>"Pat Monteseian", :occupation => "Staff"}, 
-    {:name =>"Kate Gompert", :occupation => "None"}, 
-    {:name =>"Bruce Green", :occupation => "Fan of Mildred"}
+  new_array = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G,
     ]
-end
+  end
 
 def literal_aoh
-  constant_peeps = [
+  literal_aoh = [
     {:name =>"Don Gately", :occupation => "Live-in Staff"},
     {:name =>"Joelle van Dyne", :occupation => "Radio Personality"}, 
     {:name =>"Pat Monteseian", :occupation => "Staff"}, 
